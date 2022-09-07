3c6a30dc534a29f79ead726d34930253bafc403f
Júlio Cesar Bueno Cotta <julio.buenocotta@veepee.com>
2022-09-07 22:00:49 +0200

    update output dir

--- run_benckmark.sh
+++ run_benckmark.sh
@@ -1,6 +1,6 @@
 	gradle-profiler \
 		--benchmark --project-dir "." \
 		--scenario-file basic_build.scenario \
-		--output-dir "baseline" \
+		--output-dir "ksp" \
 		--iterations 50 \
 		--warmups 3
