40fb02f89327ce5206004c88657985068fe97fea
Júlio Cesar Bueno Cotta <julio.buenocotta@veepee.com>
2022-10-03 14:40:42 +0200

    fix

--- run_benckmark.sh
+++ run_benckmark.sh
@@ -1,6 +1,6 @@
 	gradle-profiler \
 		--benchmark --project-dir "." \
-		--scenario-file basic_build.scenario \
+		--scenario-file changes.scenario \
 		--output-dir "kapt" \
 		--iterations 50 \
 		--warmups 3
